afdeed44ecfcb7ceff35a1e36afe0b3bf7136373
Steve Schmerler <mefx@gmx.net>
2012-04-20 10:40:47 +0200

    API: install.sh: Rename -p|--bin-prefix -> -b|--bin-path and don't      automatically append bin/ to that path, which is really stupid.      Rename -c|--config-prefix -> -c|--config-path .

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 prog=$(basename $0)
 simulate=false
-bin_prefix=$HOME/soft
-config_prefix=$HOME
+bin_path=$HOME/soft/bin
+config_path=$HOME
 config_file_start="."
 usage(){
     cat <<EOF
@@ -13,27 +13,28 @@ options:
 --------
 -h : help
 -s : simulate
--p | --bin-prefix : executable scripts will be copied to <bin_prefix>/bin/
-    [$bin_prefix]
--c | --config-prefix : config files will be copied to <config_prefix>/
-    [$config_prefix]
--C | --config-file-start : prepend this char to each config file [$config_file_start]
+-b | --bin-path : executable scripts will be copied to <bin_path>/
+    [$bin_path]
+-c | --config-path : config files will be copied to <config_path>/
+    [$config_path]
+-C | --config-file-start : prepend this char to each config file
+    [$config_file_start]
 EOF
 }
 
-cmdline=$(getopt -o hsp:c:C: -l bin-prefix:config-prefix:config-file-start: -- "$@")
+cmdline=$(getopt -o hsb:c:C: -l bin-path:config-path:config-file-start: -- "$@")
 eval set -- "$cmdline"
 while [ $# -gt 0 ]; do
     case "$1" in 
         -s)
             simulate=true
             ;;
-        -p|--bin-prefix)
-            bin_prefix=$2
+        -b|--bin-path)
+            bin_path=$2
             shift
             ;;
-        -c|--config-prefix)
-            config_prefix=$2
+        -c|--config-path)
+            config_path=$2
             shift
             ;;
         -C|--config-file-start)
@@ -56,18 +57,17 @@ while [ $# -gt 0 ]; do
     shift
 done
 
-bin_tgt=$bin_prefix/bin
-for dr in $bin_tgt $config_prefix; do
+for dr in $bin_path $config_path; do
     if ! [ -d $dr ]; then 
         echo "error: $dr doen't exist"
         exit 1
     fi    
 done
 
-cmd="cp -v bin/* $bin_tgt/;"
+cmd="cp -v bin/* $bin_path/;"
 for ff in $(ls config/); do
     src=config/$ff
-    tgt=$config_prefix/${config_file_start}$ff
+    tgt=$config_path/${config_file_start}$ff
     cmd=$cmd" if [ -f $tgt ]; then bash bin/backup.sh -p '.bak' $tgt; fi; \
         cp -v $src $tgt;"
 done
