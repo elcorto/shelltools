6706eae8d224fd623820f0c7c177cec596ec1f2d
Steve Schmerler <mefx@gmx.net>
2012-06-08 14:05:00 +0200

    ENH: bin/hg-qdiffstat.sh: Add -v option.

--- bin/hg-qdiffstat.sh
+++ bin/hg-qdiffstat.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# Apply each patch and print compact form of "hg qdi --st". This is for quickly
-# finding which patches change which files.
+# Apply each patch and do ``hg qdi --st``. This is for quickly finding out
+# which patches change which files.
+#
+# Use the "-v" option to see the ``hg qheader`` as well.
+
+verbose=false
+if [ "$1" = "-v" ]; then
+    verbose=true
+fi
 
 for pn in $(hg qseries); do 
-    echo -e "\n\n=============== $pn ===============" 
+    echo -e "\n\n=============== $pn ==============="
     hg qgoto $pn
-    hg qdi --st 
+    hg qdi --st
+    if $verbose; then
+        echo " "
+        hg qheader
+    fi        
 done | egrep -v 'applying|popping|now at|files changed'
