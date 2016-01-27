34e610b346472790021067759f02edbf34e2627a
Steve Schmerler <mefx@gmx.net>
2016-01-27 17:28:50 +0100

    ENH: git-checkrepo.sh: more verbose output, better indentation, remove --new
    Also fix some bugs in the branch handling logic.

--- bin/git-checkrepo.sh
+++ bin/git-checkrepo.sh
@@ -1,38 +1,99 @@
 #!/bin/sh
 
+set -e
+
+# single whitespace unit for output
+# usage: 
+#   echo "${ws}${ws}indented text"
+#   echo "indented text" | indent 2
+ws="  "
+
 rungit(){
-    $gitcmd $@ 2>&1 | sed 's/^\s*/    /g'     
+    $gitcmd $@ 2>&1
+}
+
+err(){
+    echo "error: $@"
+    exit 1
+}    
+
+indent(){
+    # indent text on stdin
+    [ $# -eq 1 ] || err "indent: need to define whitespace"
+    wundefined='#####'
+    w=$wundefined
+    [ $1 -eq 1 ] && w=${ws}
+    [ $1 -eq 2 ] && w=${ws}${ws}
+    [ $1 -eq 3 ] && w=${ws}${ws}${ws}
+    [ $1 -eq 4 ] && w=${ws}${ws}${ws}${ws}
+    [ "$w" = "${wundefined}" ] && err "indent: whitespace count $1 not supported"
+    cat | sed -re "s/^\\s*/$w/g"
+}
+
+mark(){
+    cat | sed 's/^/=> /g'
 }
 
 gogo(){
     local dr=$1
     gitcmd="git -C $dr"
-    echo "repo: $dr"
-    rungit "status --porcelain"
     remotes=$($gitcmd remote)
+    
+    echo "repo: $dr"
+    echo "${ws}status:"
+    rungit "status --porcelain" | mark | indent 2
+    
+    if $fetch; then
+        echo "${ws}fetch:"        
+        for rr in $remotes; do
+            echo "${ws}${ws}remote: $rr"
+            rungit fetch $rr | mark | indent 3
+        done
+    fi
+    
+    echo "${ws}branches sync status:"        
+    rungit branch -vva | grep -E 'ahead|behind' | mark | indent 2
+    
+    echo "${ws}local branches:"        
+    # master
+    # develop
+    # feature-foo
+    # feature/bar
+    # some-test
     local_br=$($gitcmd branch -av | grep -v remotes \
         | sed -re 's/^(\s|\*)*//g'  | mawk '{print $1}')
-    remote_br=$($gitcmd branch -rv | mawk '{print $1}' | cut -d/ -f2 \
+    # origin/master        
+    # origin/develop
+    # origin/feature-foo
+    # origin/feature/bar
+    # dev/some-test
+    remote_br_full=$($gitcmd branch -rv | mawk '{print $1}' \
         | grep -v HEAD | sort -u)
-    if $fetch; then
-        for rr in $remotes; do
-            echo "  remote: $rr"
-            rungit fetch $rr
-        done
-    fi      
-    rungit branch -vva | grep -E 'ahead|behind'
     for lb in $local_br; do
-        if ! echo "$remote_br" | grep -q $lb; then
-            echo "    unpushed local branch: $lb"
+        if ! echo "$remote_br_full" | grep -q $lb; then
+            echo "local-only branch: $lb" | mark | indent 3
         fi    
     done
-    if $show_new_remote_bf; then
-        for rb in $remote_br; do
+    
+    echo "${ws}remote branches:"        
+    for rr in $remotes; do
+        echo "${ws}${ws}remote: $rr"
+        # rr=origin
+        #   master        
+        #   develop
+        #   feature-foo
+        #   feature/bar
+        # rr=dev
+        #   some-test
+        remote_br_rr=$($gitcmd branch -rv | mawk "/^[ ]+$rr/ {print \$1}" \
+            | sed -re "s|$rr/||g" | grep -v HEAD | sort -u)
+        for rb in $remote_br_rr; do 
+            echo "branch: $rb" | indent 3
             if ! echo "$local_br" | grep -q $rb; then
-                echo "    new remote branch:     $rb"
+                echo "new remote branch: $rr/$rb" | mark | indent 4
             fi    
-        done    
-    fi      
+        done
+    done
 }
 
 
@@ -42,8 +103,8 @@ usage(){
     cat << eof
 In each git repo, run "git fetch" for each remote (e.g. mostly only origin,
 probably) if --fetch. Then show if we are ahead/behind in some branch for that
-remote. Also, show unpushed local branches, and new remote branches with --new
-(assumtion: branches have the same name here and there).
+remote. Also, show local-only branches, and new remote branches (assumtion:
+branches have the same name here and there). All infos are marked with "=>".
 
 usage
 -----
@@ -54,19 +115,14 @@ Default is the current dir.
 options
 -------
 -f / --fetch : run "git fetch <remote>" at first
--r / --new : show new remote branches
 eof
 }
 
-show_new_remote_bf=false
 fetch=false
 cmdline=$(getopt -o hrf -l new,fetch -n $prog -- "$@")
 eval set -- "$cmdline"
 while [ $# -gt 0 ]; do
     case $1 in
-        -r | --new)
-            show_new_remote_bf=true
-            ;;
         -f | --fetch)
             fetch=true
             ;;
@@ -91,5 +147,6 @@ if [ $# -eq 0 ]; then
 else    
     for dr in $@; do
         gogo $dr
+        echo ""
     done
 fi    
