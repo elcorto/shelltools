84b937769c5c395f9462fe90181b444f69b1c3bd
Steve Schmerler <mefx@gmx.net>
2016-03-03 18:13:57 +0100

    ENH: rst2redmime-textile.sh: add -h/--help

--- bin/rst2redmime-textile.sh
+++ bin/rst2redmime-textile.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+if echo "$@" | grep -qEe '-h|--help'; then
+    cat << EOF
+usage:
+    $(basename $0) file.rst
+EOF
+    exit 0
+fi
+
 pandoc -f rst -t textile $1 | pandoc-fix-textile.py
