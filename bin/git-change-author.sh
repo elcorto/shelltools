f606ef8b50ab186653ef0697c8a917056d21eadf
Steve Schmerler <mefx@gmx.net>
2016-04-10 19:25:17 +0200

    ENH: git-change-author.sh can set author/committer name as well
    Add -e and -n option to choose between email and name mode. Use better
    quoting in the executed git filter-branch command.

--- bin/git-change-author.sh
+++ bin/git-change-author.sh
@@ -4,8 +4,13 @@ prog=$(basename $0)
 
 usage(){
     cat << eof
-Change <oldmail> to <newmail> in git history. Change GIT_AUTHOR_EMAIL and if
--c is used also GIT_COMMITTER_EMAIL.
+Change git author/committer name/email. 
+
+option      value changes                            
+-e          GIT_AUTHOR_EMAIL        
+-n          GIT_AUTHOR_NAME
+-c -e       GIT_COMMITTER_EMAIL
+-c -n       GIT_COMMITTER_NAME
 
 Test if it worked with "$prog -l" afterwards. 
 
@@ -14,20 +19,41 @@ needed to be changed.
 
 usage
 -----
-$prog -l | [-c] <oldmail> <newmail>
+$prog -l | [-c] [-e | -n] <oldval> <newval>
+
+arguments
+---------
+oldval, newval : old/new email (-e) or name (-n), use proper quoting
 
 options
 -------
--l : only list available mails
--c : set GIT_COMMITTER_EMAIL as well
+-l : only list available authors and emails
+-c : set GIT_COMMITTER_{NAME,EMAIL} as well
 
-Notes
+examples
+--------
+List.
+    $prog -l
+
+Author mail.
+    $prog -e old@mail.com new@mail.com
+
+Author and committer mail.
+    $prog -c -e old@mail.com new@mail.com
+
+Set author mail where there is none.
+    $prog -e '' new@mail.com
+
+Set new author and committer name.
+    $prog -c -n john 'John Doe'
+
+notes
 -----
 You may want to do this after hg-fast-export, such as
     
     $ cat ~/map
     john = John Doe <mail@doe.com>
-    gf = Gaylord Focker <gay@focker.org>
+    gf = Gaylord Focker <gl@focker.org>
     $ cd git-repo
     $ git init 
     $ hg-fast-export -r /path/to/hg-repo -A ~/map
@@ -42,9 +68,16 @@ dir (a message is printed in that case).
 eof
 }
 
+
 list=false
 committer=false
-cmdline=$(getopt -o hlc -n $prog -- "$@")
+cmdline=$(getopt -o hlcen -n $prog -- "$@")
+oldmail=
+newmail=
+oldname=
+newname=
+mode_mail=false
+mode_name=false
 eval set -- "$cmdline"
 while [ $# -gt 0 ]; do
     case $1 in
@@ -53,6 +86,12 @@ while [ $# -gt 0 ]; do
             ;;
         -c)
             committer=true
+            ;;
+        -e)
+            mode_mail=true
+            ;;
+        -n)
+            mode_name=true
             ;;
         -h)
             usage
@@ -87,21 +126,34 @@ if [ $# -eq 0 ]; then
     exit 1
 fi    
 
-oldmail=$1
-newmail=$2
+oldval=$1
+newval=$2
 
 cmd="
-if [ \$GIT_AUTHOR_EMAIL = $oldmail ]; then
-        export GIT_AUTHOR_EMAIL=$newmail
+if $mode_mail; then
+    if [ \"\$GIT_AUTHOR_EMAIL\" = \"$oldval\" ]; then
+        export GIT_AUTHOR_EMAIL=\"$newval\"
+    fi
+elif $mode_name; then
+    if [ \"\$GIT_AUTHOR_NAME\" = \"$oldval\" ]; then
+        export GIT_AUTHOR_NAME=\"$newval\"
+    fi
 fi
 "
 
 if $committer; then
-cmd="$cmd
-if [ \$GIT_COMMITTER_EMAIL = $oldmail ]; then
-        export GIT_COMMITTER_EMAIL=$newmail
+    cmd="$cmd
+if $mode_mail; then
+    if [ \"\$GIT_COMMITTER_EMAIL\" = \"$oldval\" ]; then
+        export GIT_COMMITTER_EMAIL=\"$newval\"
+    fi
+elif $mode_name; then
+    if [ \"\$GIT_COMMITTER_NAME\" = \"$oldval\" ]; then
+        export GIT_COMMITTER_NAME=\"$newval\"
+    fi
 fi
 "
 fi
 
-git filter-branch --env-filter "$cmd" -- --all
+# https://help.github.com/articles/changing-author-info/
+git filter-branch --env-filter "$cmd" --tag-name-filter cat -- --all
