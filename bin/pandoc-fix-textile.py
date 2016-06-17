58b0f6bd682977cbf84f397c2cc77c17eb1a17b6
Steve Schmerler <mefx@gmx.net>
2016-06-17 17:42:49 +0200

    ENH: pandoc-fix-textile.py: Support nested lists.
    This is a hack by the book: fiddle out html parts, use pandoc again to
    convert to markdown_strict, and from that back to textile. I am not proud of
    that (well, maybe a little), but It Works(tm).

--- bin/pandoc-fix-textile.py
+++ bin/pandoc-fix-textile.py
@@ -22,43 +22,120 @@ two lines with blank::
 
 [[wiki link must be unchanged]]
 
-* bullet
-* list
+* this is a 
+* nested
+    * bullet
+    * list
 
 .. this is a comment and will be ignored
 ----------------------------------------------------
+
+Stuff we fix
+============
+
+bc blocks
+---------
+
+rst:
+
+    ::
+        foo
+        bar
+        baz
+
+wrong:
+
+    bc. foo
+    bar
+    baz
+
+right:
+
+    <pre>
+    foo
+    bar
+    baz
+    </pre>
+
+
+html
+----
+All html stuff starts with "<.." on every line. This is good for parsing, i.e.
+we don't have to fix things like
+
+<foo>
+...
+</foo>
+
+rst:
+
+    * foo
+        * bar
+        * baz
+
+wrong:
+
+    <ul>
+    <dt>foo<dt>
+    <dd><ul>
+    <li>bar</li>
+    <li>baz</li>
+    </ul>
+    </dd>
+    </ul>
+... or similar, I don't speak html much.
+
+right:
+
+    * foo
+    ** bar
+    ** baz
+
+random utf8 hickups
+-------------------
+see `repl`
 """
 
 
 import sys, argparse
+from subprocess import Popen, PIPE
 from argparse import RawTextHelpFormatter
 
 desc = "Fix pandoc (rst to) textile markup conversion for redmine."
 usage = "pandoc -f <format> -t textile <file> | %(prog)s"
-epilog = """
-known issues:
-* multi-level bullet lists not supported
-"""
 
 parser = argparse.ArgumentParser(description=desc, usage=usage,
-                                 epilog=epilog, 
 	                         formatter_class=RawTextHelpFormatter)
 args = parser.parse_args()
 
 txt = ''
-inblock = False
+html = ''
+inblock_bc = False
+inblock_html = False
 for line in sys.stdin:
     if line.startswith('bc.'):
-        inblock = True            
+        inblock_bc = True            
         txt += "<pre>\n"
         txt += line.strip().replace('bc. ','') + '\n'
         continue
-    if inblock:
+    elif line.startswith('<') and not 'pre>' in line:
+        inblock_html = True
+    if inblock_bc:
         if line.strip() == "":
             txt += "</pre>\n"
-            inblock = False
+            inblock_bc = False
         else:                
             txt += line
+    elif inblock_html:
+        if not line.startswith('<'):
+            inblock_html = False
+            pp = Popen('pandoc -f html -t markdown_strict | pandoc -f markdown_strict -t textile', 
+                       stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True)
+            stdout = pp.communicate(html)[0]
+            txt += stdout
+            html = ''
+        else:                
+            html += line
     else:
         txt += line
 
@@ -73,6 +150,7 @@ repl = {r'&lt;': '<',
         r'&#45;': '-',
         r'&#43;': '+',
         r'&#42;': '*',
+        r'&#124;': '|',
         r'"$":': '',
         r'&amp;': '&',
         }
@@ -80,3 +158,4 @@ repl = {r'&lt;': '<',
 for key,val in repl.iteritems():
     txt = txt.replace(key, val)
 print(txt)               
+
