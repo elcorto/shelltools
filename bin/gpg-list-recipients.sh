121b22605bd1226a941650337178b8f3de78f4da
Steve Schmerler <mefx@gmx.net>
2016-07-15 13:33:11 +0200

    DOC: Comment in gpg-list-recipients.sh

--- bin/gpg-list-recipients.sh
+++ bin/gpg-list-recipients.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+# Note that "gpg2 --list-only" does exactly the same now.
+
 #http://blog.endpoint.com/2013/05/gnupg-list-all-recipients-of-message.html
 gpg --list-only --no-default-keyring --secret-keyring /dev/null $@
