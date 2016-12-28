88a0d5ca5f4dea9885cd269837f2ae63bba14e17
Steve Schmerler <git@elcorto.com>
2016-12-28 06:03:06 +0100

    ENH: make xvim work with zsh profile
    Source .profile_common, which contains our shell-independent env
    settings. Don't source bash files anymore.

--- bin/its-all-text-xvim.sh
+++ bin/its-all-text-xvim.sh
@@ -1,6 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
 # Wrapper for xvim, used as editor for Firefox' "It's All Text!" plugin.
 
-. $HOME/.profile
-xvim -c \'set tw=1000\' $@
+##debug=true
+debug=false
+
+if $debug; then
+    debug_log=/tmp/$(basename $0).$$
+    echo '' > $debug_log
+    exec >>$debug_log
+    exec 2>>$debug_log
+fi
+
+$debug && echo "$0: start xvim ..."
+$(dirname $0)/xvim -c \'set tw=1000\' $@
+$debug && echo "$0: ok"
+
+exit 0
