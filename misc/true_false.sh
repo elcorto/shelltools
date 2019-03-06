d051bcf88a85649d210ef8de15de8565b9275003
Steve Schmerler <git@elcorto.com>
2019-03-06 15:03:55 +0100

    ENH: update misc/true_false.sh: functions returning "bools"

--- misc/true_false.sh
+++ misc/true_false.sh
@@ -1,18 +1,29 @@
 # The final and definitive way to handle bool stuff in POSIX shell [1]. Using
 # command chains such as [...] && [...], each [...] (or test ...) evaluates to
 # 0 (true) or != 0 (false). The shell builtins true/false evaluate to 0/1 (as
-# in a=true). 
+# in a=true).
 #
 # [1] http://pubs.opengroup.org/onlinepubs/9699919799/xrat/V4_xcu_chap02.html
+
+header(){
+    cat << eof
+
+$@
+
+eof
+}
 
 exe(){
     cmd="if $1; then echo 'true'; else echo 'false'; fi"
     echo "${1}: $(eval $cmd) $2"
 }
 
-echo ""
-echo "always use one of these:"
-echo ""
+exe_pre(){
+    cmd="$1; if $2; then echo 'true'; else echo 'false'; fi"
+    echo "$1; ${2}: $(eval $cmd) $3"
+}
+
+header "always use one of these:"
 
 exe '[ 1 -eq 1 -a 1 -eq 1 ]'
 exe '[ 1 -eq 1 -a 1 -eq 2 ]'
@@ -25,7 +36,7 @@ exe '[ 1 -eq 2 -o 1 -eq 2 ]'
 exe '[ \( 1 -eq 1 \) -a \( 1 -eq 1 \) ]'
 exe '[ \( 1 -eq 1 \) -a \( 1 -eq 2 \) ]'
 exe '[ \( 1 -eq 2 \) -a \( 1 -eq 2 \) ]'
-                                      
+
 exe '[ \( 1 -eq 1 \) -o \( 1 -eq 1 \) ]'
 exe '[ \( 1 -eq 1 \) -o \( 1 -eq 2 \) ]'
 exe '[ \( 1 -eq 2 \) -o \( 1 -eq 2 \) ]'
@@ -38,9 +49,8 @@ exe '[ 1 -eq 1 ] || [ 1 -eq 1 ]'
 exe '[ 1 -eq 1 ] || [ 1 -eq 2 ]'
 exe '[ 1 -eq 2 ] || [ 1 -eq 2 ]'
 
-echo ""
-echo "using builtins true/false:"
-echo ""
+
+header "using builtins true/false:"
 
 exe 'true && true'
 exe 'true && false'
@@ -49,9 +59,8 @@ exe 'true || true'
 exe 'true || false'
 exe 'false || false'
 
-echo ""
-echo "sanity check:"
-echo ""
+
+header "sanity check:"
 
 exe '[ 1 -eq 1 -a 2 -eq 1 ]'
 exe '[ 2 -eq 1 -a 1 -eq 2 ]'
@@ -61,10 +70,44 @@ exe '[ 1 -eq 1 -o 2 -eq 1 ]'
 exe '[ 2 -eq 1 -o 1 -eq 2 ]'
 exe '[ 2 -eq 1 -o 2 -eq 1 ]'
 
-echo ""
-echo "true/false builtin subtleties:"
-echo ""
+
+header "true/false builtin subtleties:"
+
 exe '[ 1 -eq 2 ] || [ 1 -eq 2 ]' "# ok, valid"
 exe 'false || false' "            # ok, same as above"
 exe '[ 1 -eq 2 -o 1 -eq 2 ]' "    # ok, same as above"
-exe '[ false -o false ]' "        # NOT the same; this is equal to [ 0 -o 0 ], [ 1 -o 1 ], ..."
+exe '[ false -o false ]' "        # WRONG; this is equal to [ 0 -o 0 ], [ 1 -o 1 ], ..."
+
+header "functions: never return builtin true/false, this works only in zsh"
+
+##setup='t(){ return true; }; f(){ return false; }'
+##exe_pre "$setup" 't'
+##exe_pre "$setup" '$(t)'
+##exe_pre "$setup" 'f'
+##exe_pre "$setup" '$(f)'
+
+header "functions: always return 0/1"
+
+setup='zero(){ return 0; }; one(){ return 1; }'
+exe_pre "$setup" 'zero'
+exe_pre "$setup" '$(zero)'
+exe_pre "$setup" 'one'
+exe_pre "$setup" '$(one)'
+
+exe_pre "$setup" 'zero && true'
+exe_pre "$setup" '$(zero) && true'
+exe_pre "$setup" 'one && true'
+exe_pre "$setup" '$(one) && true'
+
+#./true_false.sh: 1: [: Illegal number: zero
+##exe_pre "$setup" '[ zero -eq 0 ]'
+
+#./true_false.sh: 1: [: -eq: unexpected operator
+##exe_pre "$setup" '[ $(zero) -eq 0 ]'
+
+#./true_false.sh: 1: [: -eq: unexpected operator
+##exe_pre "$setup; ret=\$(zero)" '[ $ret -eq 0 ]'
+
+exe_pre "$setup; zero" '[ $? -eq 0 ]'
+exe_pre "$setup; zero; ret=\$?" '[ $ret -eq 0 ]'
+
