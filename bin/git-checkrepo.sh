5d320df972b8230eb15d187311abf66942351611
Steve Schmerler <git@elcorto.com>
2019-06-04 13:52:17 +0200

    MNT: cleanup in git-checkrepo.sh

--- bin/git-checkrepo.sh
+++ bin/git-checkrepo.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-set -e
+set -eu
 
 # single whitespace unit for output
-# usage: 
+# usage:
 #   echo "${ws}${ws}indented text"
 #   echo "indented text" | indent 2
 ws="  "
@@ -17,7 +17,7 @@ rungit(){
 err(){
     echo "error: $@"
     exit 1
-}    
+}
 
 indent(){
     # indent text on stdin
@@ -39,29 +39,29 @@ mark(){
 gogo(){
     local dr=$1
     [ -d $dr/.git ] && _gogo $dr || echo "skip: $dr"
-}    
+}
 
 _gogo(){
     local dr=$1
     gitcmd="git -C $dr"
     remotes=$($gitcmd remote)
-    
+
     echo "repo: $dr"
     echo "${ws}status:"
     rungit "status --porcelain" | mark | indent 2
-    
+
     if $fetch; then
-        echo "${ws}fetch:"        
+        echo "${ws}fetch:"
         for rr in $remotes; do
             echo "${ws}${ws}remote: $rr"
             rungit fetch $rr | mark | indent 3
         done
     fi
-    
-    echo "${ws}branches sync status:"        
+
+    echo "${ws}branches sync status:"
     rungit branch -vva | grep -E 'ahead|behind' | tr -s ' ' | mark | indent 2
-    
-    echo "${ws}local branches:"        
+
+    echo "${ws}local branches:"
     # master
     # develop
     # feature-foo
@@ -69,7 +69,7 @@ _gogo(){
     # some-test
     local_br=$($gitcmd branch -av | grep -v remotes \
         | sed -re 's/^(\s|\*)*//g'  | mawk '{print $1}')
-    # origin/master        
+    # origin/master
     # origin/develop
     # origin/feature-foo
     # origin/feature/bar
@@ -79,14 +79,14 @@ _gogo(){
     for lb in $local_br; do
         if ! echo "$remote_br_full" | grep -q $lb; then
             echo "local-only branch: $lb" | mark | indent 2
-        fi    
+        fi
     done
-    
-    echo "${ws}remote branches:"        
+
+    echo "${ws}remote branches:"
     for rr in $remotes; do
         echo "${ws}${ws}remote: $rr"
         # rr=origin
-        #   master        
+        #   master
         #   develop
         #   feature-foo
         #   feature/bar
@@ -94,12 +94,12 @@ _gogo(){
         #   some-test
         remote_br_rr=$($gitcmd branch -rv | mawk "/^[ ]+$rr/ {print \$1}" \
             | sed -re "s|$rr/||g" | grep -v HEAD | sort -u)
-        for rb in $remote_br_rr; do 
+        for rb in $remote_br_rr; do
             if echo "$local_br" | grep -q $rb; then
                 echo "branch: $rb" | indent 3
             else
                 echo "new branch: $rb" | mark | indent 3
-            fi    
+            fi
         done
     done
 }
@@ -110,7 +110,7 @@ prog=$(basename $0)
 usage(){
     cat << eof
 In each git repo, run "git fetch" for each remote (e.g. mostly only origin,
-probably) if --fetch. Then show if we are ahead/behind in some branch for that
+probably) if -f (fetch). Then show if we are ahead/behind in some branch for that
 remote. Also, show local-only branches, and new remote branches (assumtion:
 branches have the same name here and there). All infos are marked with "=>".
 
@@ -122,18 +122,18 @@ Default is the current dir.
 
 options
 -------
--f / --fetch : run "git fetch <remote>" at first
+-f : run "git fetch <remote>" at first
 -F : fork
 eof
 }
 
 fetch=false
 fork=false
-cmdline=$(getopt -o hrfF -l new,fetch -n $prog -- "$@")
+cmdline=$(getopt -o hfF -n $prog -- "$@")
 eval set -- "$cmdline"
 while [ $# -gt 0 ]; do
     case $1 in
-        -f | --fetch)
+        -f)
             fetch=true
             ;;
         -F)
@@ -147,7 +147,7 @@ while [ $# -gt 0 ]; do
             shift
             break
             ;;
-        *)  
+        *)
             echo "Cmd line error! Grab a coffee and recompile your kernel :)"
             exit 1
             ;;
@@ -157,15 +157,15 @@ done
 
 if [ $# -eq 0 ]; then
     gogo .
-else    
+else
     for dr in $@; do
         if $fork; then
-            (txt=$(gogo $dr); 
+            (txt=$(gogo $dr);
              echo "$txt";
              echo "") &
         else
             gogo $dr
             echo ""
-        fi    
+        fi
     done
-fi 
+fi
