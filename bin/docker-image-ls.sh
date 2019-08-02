471ee5bd9a611ccfbac3cab1eb9cab5e277e8afc
Steve Schmerler <git@elcorto.com>
2019-08-02 14:25:42 +0200

    BEH: docker-image-ls.sh: CreatedAt -> CreatedSince
    CreatedSince is more useful in most cases.

--- bin/docker-image-ls.sh
+++ bin/docker-image-ls.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 fmt="{{.Repository}}:{{.Tag}}"
-[ "$1" = "-v" ] && fmt="{{.Repository}}:{{.Tag}} {{.ID}} {{.CreatedAt}} {{.Size}}"
+[ "$1" = "-v" ] && fmt="{{.Repository}}:{{.Tag}} {{.ID}} {{.CreatedSince}} {{.Size}}"
 
 docker image ls --format "$fmt" | column -t
